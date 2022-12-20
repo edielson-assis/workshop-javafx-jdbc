1280bf207c735b2092dc7a7d1791154fedd93c9d
Edielson Assis <edielsonnice@hotmail.com>
2022-12-20 14:52:13 -0300

    Adding database access

---
+++ src/db/path/PropertiesPath.java
@@ -0,0 +1,8 @@
+package db.path;
+
+final public class PropertiesPath {
+    
+    final public static String fullPath() {
+        return "C:\\Codigos\\Udemy\\Java_oo\\workshop-javafx-jdbc\\src\\db.properties";
+    }
+}
